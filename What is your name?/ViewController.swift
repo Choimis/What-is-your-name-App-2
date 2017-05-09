a8dabc7032a26aa12b156ffc2797080e92306409
Daniel O'Brien <daniel@Daniels-MacBook-Air.local>
2017-05-09 18:44:12 +0100

    Initial Commit

---
+++ What is your name?/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  What is your name?
+//
+//  Created by Daniel O'Brien on 09/05/2017.
+//  Copyright © 2017 Daniel O'Brien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
